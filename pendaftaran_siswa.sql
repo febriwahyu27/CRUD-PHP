92778e107c1b5045cbed1c79fc054143505974e3
febriwahyu27 <febritriwahyu@gmail.com>
2022-01-03 01:51:28 +0700

    project kedua

---
+++ pendaftaran_siswa.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Jan 2022 pada 19.48
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_siswa`
+--
+
+CREATE TABLE `calon_siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(225) NOT NULL,
+  `agama` varchar(225) NOT NULL,
+  `sekolah_asal` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `calon_siswa`
+--
+
+INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
+(1, 'febriwahyu', 'jepara', 'laki-laki', 'Islam', 'smp 6 jepara');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
